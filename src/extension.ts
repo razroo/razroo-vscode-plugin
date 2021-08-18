7c3ca6283de51c1183a4c4df25d59eb85279e925
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2021-08-18 08:30:10 -0300

    This commit adds a new command for the authentication with VS Code and the front end, in addition adds a function that check if the token is saved in DynamoDB

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
 import open = require('open');
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
 import * as AdmZip from 'adm-zip';
 import * as request from 'request';
 import * as http from 'http2';
 
-import { getAuth0Url } from './utils';
-import { AUTH0URL, COMMAND_AUTH0_AUTH, DEVAUTHURL, MEMENTO_RAZROO_ACCESS_TOKEN, MEMENTO_RAZROO_ID_TOKEN, MEMENTO_RAZROO_LOGIN_SOCKET_CHANNEL, MEMENTO_RAZROO_REFRESH_TOKEN, SOCKET_HOST } from './constants';
-
+import { existVSCodeAuthenticate, getAuth0Url } from './utils';
+import {
+  AUTH0URL,
+  COMMAND_AUTH0_AUTH,
+  DEVAUTHURL,
+  MEMENTO_RAZROO_ACCESS_TOKEN,
+  MEMENTO_RAZROO_ID_TOKEN,
+  MEMENTO_RAZROO_LOGIN_SOCKET_CHANNEL,
+  MEMENTO_RAZROO_REFRESH_TOKEN,
+  SOCKET_HOST,
+} from './constants';
 
 const showErrorMessage = vscode.window.showErrorMessage;
 const showInformationMessage = vscode.window.showInformationMessage;
@@ -19,7 +27,6 @@ const showOpenDialog = vscode.window.showOpenDialog;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
@@ -44,116 +51,206 @@ export async function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-
-
-  const auth0Authentication = vscode.commands.registerCommand(COMMAND_AUTH0_AUTH,
+  const auth0Authentication = vscode.commands.registerCommand(
+    COMMAND_AUTH0_AUTH,
     async () => {
-      console.log("inside auth0Authentcation");
+      console.log('inside auth0Authentcation');
       //Auth0 Authentication
       // const githubEmail = await vscode.window.showInputBox({ title: "Your GitHub Email", placeHolder: "Your Github email", prompt: "Please type in your Github email", validateInput: (value) => validateEmail(value) });
       // console.log("githubEmail", githubEmail);
-      const token =  uuidv4();
+      const token = uuidv4();
       const host = SOCKET_HOST;
       const loginUrl = getAuth0Url(token, host);
 
-      const httpServer = createServer();      
+      const httpServer = createServer();
       const io = new Server(httpServer, {
-          cors: {
-              origin: [DEVAUTHURL, AUTH0URL],
-              methods: ["GET", "POST"]
-          }
+        cors: {
+          origin: [DEVAUTHURL, AUTH0URL],
+          methods: ['GET', 'POST'],
+        },
       });
 
-      io.on("connection", (socket: Socket) => {
-          socket.on(token, (msg) => {
-            const [refresh_token, id_token, access_token] = msg;
-            showInformationMessage('User is authenticated via web.');
-            context.workspaceState.update(MEMENTO_RAZROO_REFRESH_TOKEN, refresh_token);
-            context.workspaceState.update(MEMENTO_RAZROO_ACCESS_TOKEN, access_token);
-            context.workspaceState.update(MEMENTO_RAZROO_ID_TOKEN, id_token);
-            context.workspaceState.update(MEMENTO_RAZROO_LOGIN_SOCKET_CHANNEL, token);
-          });
+      io.on('connection', (socket: Socket) => {
+        socket.on(token, (msg) => {
+          const [refresh_token, id_token, access_token] = msg;
+          showInformationMessage('User is authenticated via web.');
+          context.workspaceState.update(
+            MEMENTO_RAZROO_REFRESH_TOKEN,
+            refresh_token
+          );
+          context.workspaceState.update(
+            MEMENTO_RAZROO_ACCESS_TOKEN,
+            access_token
+          );
+          context.workspaceState.update(MEMENTO_RAZROO_ID_TOKEN, id_token);
+          context.workspaceState.update(
+            MEMENTO_RAZROO_LOGIN_SOCKET_CHANNEL,
+            token
+          );
+        });
+      });
+      httpServer.listen(3000);
+
+      await open(loginUrl);
+    }
+  );
+  context.subscriptions.push(auth0Authentication);
+
+  const NewAuth0Authentication = vscode.commands.registerCommand(
+    'extension.NewAuth0Authentication',
+    async () => {
+      console.log('inside auth0Authentcation');
+
+      const token = uuidv4();
+      const host = SOCKET_HOST;
+      const loginUrl = getAuth0Url(token, host);
+
+      const httpServer = createServer();
+      const io = new Server(httpServer, {
+        cors: {
+          origin: [DEVAUTHURL, AUTH0URL],
+          methods: ['GET', 'POST'],
+        },
+      });
+
+      io.on('connection', (socket: Socket) => {
+        socket.on(token, (msg) => {
+          const [refresh_token, id_token, access_token] = msg;
+          showInformationMessage('User is authenticated via web.');
+          console.log('refresh_token', refresh_token);
+          console.log('id_token', id_token);
+          console.log('access_token', access_token);
+          context.workspaceState.update(
+            MEMENTO_RAZROO_REFRESH_TOKEN,
+            refresh_token
+          );
+          context.workspaceState.update(
+            MEMENTO_RAZROO_ACCESS_TOKEN,
+            access_token
+          );
+          context.workspaceState.update(MEMENTO_RAZROO_ID_TOKEN, id_token);
+          context.workspaceState.update(
+            MEMENTO_RAZROO_LOGIN_SOCKET_CHANNEL,
+            token
+          );
+        });
       });
       httpServer.listen(3000);
 
       await open(loginUrl);
 
-
-    });
-  context.subscriptions.push(auth0Authentication);
-
-  const getGenerateCode = vscode.commands.registerCommand('extension.getGenerateCode', async () => {
-    // get token
-    const token = context.workspaceState.get(MEMENTO_RAZROO_ACCESS_TOKEN);
-    console.log("Token: ", token);
-    if ( !token ) {
-      console.error('Token is null');
-      showErrorMessage("Session has expired. Please login again.");
-      vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
-      return;
+      await existVSCodeAuthenticate(token);
     }
-    // generate prompt
-    const templateId = await vscode.window.showInputBox({ title: "Your templateId", placeHolder: "Your templateId", prompt: "Please type in the templateId" });
-    console.log('templateId: '+templateId);
-    
-    const url = 'https://vuerbsj4cjffvfzx7cph4iy7se.appsync-api.us-east-1.amazonaws.com/graphql';
-    const body = {
-      "query": `query generateCode{\r\n      generateCode(generateCodeParameters: {templateId: \"${templateId}\"}) {\r\n    template {\r\n      author\r\n      description\r\n      id\r\n      lastUpdated\r\n      name\r\n      parameters\r\n      stepper\r\n      type\r\n    }\r\n    downloadUrl\r\n    parameters\r\n  }\r\n}`,
-      "variables": {}
-    };
-    request.post( { url, body: JSON.stringify(body),
-      headers: {
-        Authorization: token,
-        "Content-Type": "application/json"
-      },
-      gzip: true
-    }, async (error, response, body) => {
-        // console.log("error: ",error);
-        if ( response.statusCode === http.constants.HTTP_STATUS_FORBIDDEN || response.statusCode === http.constants.HTTP_STATUS_UNAUTHORIZED ) {
-          showErrorMessage("Session has expired. Please login again.");
-          vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
-          return;
-        }
-        if ( error ) {
-          await showErrorMessage("Something went wrong. Please contact support.");
-          return;
-        }
-        
-        // console.log("Response: ", response);
-        // console.log("Body: ", body);
+  );
 
-        const bodyObject = JSON.parse(body);
+  context.subscriptions.push(NewAuth0Authentication);
 
-        request.get({url: bodyObject.data.generateCode.downloadUrl , encoding: null}, async (err, res, body) => {
-          var zip = new AdmZip(body);          
-          const defaultUri = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri : null;
-          
-          let options =  
-          defaultUri ? {canSelectFiles: false, canSelectFolders: true , canSelectMany: false,
-            defaultUri} : {canSelectFiles: false, canSelectFolders: true , canSelectMany: false };
-          
-          showOpenDialog(options).then((value: vscode.Uri[] | undefined) => {
-            if ( !value ) {
-              // console.log("User did not select a folder");
-              showInformationMessage("Please select a folder");
-              return;
+  const getGenerateCode = vscode.commands.registerCommand(
+    'extension.getGenerateCode',
+    async () => {
+      // get token
+      const token = context.workspaceState.get(MEMENTO_RAZROO_ACCESS_TOKEN);
+      console.log('Token: ', token);
+      if (!token) {
+        console.error('Token is null');
+        showErrorMessage('Session has expired. Please login again.');
+        vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
+        return;
+      }
+      // generate prompt
+      const templateId = await vscode.window.showInputBox({
+        title: 'Your templateId',
+        placeHolder: 'Your templateId',
+        prompt: 'Please type in the templateId',
+      });
+      console.log('templateId: ' + templateId);
+
+      const url =
+        'https://vuerbsj4cjffvfzx7cph4iy7se.appsync-api.us-east-1.amazonaws.com/graphql';
+      const body = {
+        query: `query generateCode{\r\n      generateCode(generateCodeParameters: {templateId: \"${templateId}\"}) {\r\n    template {\r\n      author\r\n      description\r\n      id\r\n      lastUpdated\r\n      name\r\n      parameters\r\n      stepper\r\n      type\r\n    }\r\n    downloadUrl\r\n    parameters\r\n  }\r\n}`,
+        variables: {},
+      };
+      request.post(
+        {
+          url,
+          body: JSON.stringify(body),
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+          gzip: true,
+        },
+        async (error, response, body) => {
+          // console.log("error: ",error);
+          if (
+            response.statusCode === http.constants.HTTP_STATUS_FORBIDDEN ||
+            response.statusCode === http.constants.HTTP_STATUS_UNAUTHORIZED
+          ) {
+            showErrorMessage('Session has expired. Please login again.');
+            vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
+            return;
+          }
+          if (error) {
+            await showErrorMessage(
+              'Something went wrong. Please contact support.'
+            );
+            return;
+          }
+
+          // console.log("Response: ", response);
+          // console.log("Body: ", body);
+
+          const bodyObject = JSON.parse(body);
+
+          request.get(
+            { url: bodyObject.data.generateCode.downloadUrl, encoding: null },
+            async (err, res, body) => {
+              var zip = new AdmZip(body);
+              const defaultUri = vscode.workspace.workspaceFolders
+                ? vscode.workspace.workspaceFolders[0].uri
+                : null;
+
+              let options = defaultUri
+                ? {
+                    canSelectFiles: false,
+                    canSelectFolders: true,
+                    canSelectMany: false,
+                    defaultUri,
+                  }
+                : {
+                    canSelectFiles: false,
+                    canSelectFolders: true,
+                    canSelectMany: false,
+                  };
+
+              showOpenDialog(options).then(
+                (value: vscode.Uri[] | undefined) => {
+                  if (!value) {
+                    // console.log("User did not select a folder");
+                    showInformationMessage('Please select a folder');
+                    return;
+                  }
+                  const dir = value[0];
+                  try {
+                    // console.log("Dir: ", dir.fsPath);
+                    zip.extractAllTo(dir.fsPath, false);
+                  } catch (error) {
+                    // let the user know that the download faile, check folder permission, or ask support.
+                    showErrorMessage(
+                      'We had problems writting in that folder, please check for permissions'
+                    );
+                  }
+                }
+              );
             }
-            const dir = value[0];
-            try {
-              // console.log("Dir: ", dir.fsPath);
-              zip.extractAllTo(dir.fsPath, false);  
-            }catch(error){
-              // let the user know that the download faile, check folder permission, or ask support.
-              showErrorMessage("We had problems writting in that folder, please check for permissions");
-            }
-          });
-        });
-    });
-  });
+          );
+        }
+      );
+    }
+  );
   context.subscriptions.push(getGenerateCode);
 }
 
-
-
 // this method is called when your extension is deactivated
-export function deactivate() {  }
+export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,81 @@
 import { AUTH0URL } from './constants';
+import * as request from 'request';
 
 export const validateEmail = (email: string) => {
-    const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return res.test(String(email).toLowerCase()) ? undefined : email;
+  const res =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return res.test(String(email).toLowerCase()) ? undefined : email;
 };
 
 export const getAuth0Url = (token: string, socketHost: string) => {
-    const host = process.env.scope === 'DEVELOPMENT' ? 'http://localhost:4200' : AUTH0URL;
-    const loginUrl =  `${host}?vscodeToken=${token}&socketVsCode=${socketHost}`;
-    return loginUrl;
+  const host =
+    process.env.scope === 'DEVELOPMENT' ? 'http://localhost:4200' : AUTH0URL;
+  const loginUrl = `${host}?vscodeToken=${token}&socketVsCode=${socketHost}`;
+  return loginUrl;
 };
+
+export const existVSCodeAuthenticate = async (token: string) => {
+  console.log('Start');
+  let response;
+
+  for (let i = 0; i < 1; ) {
+    // const url = 'http://localhost:3000/dev/graphql';
+    // const body = {
+    //   query: `query Query($_vsCodeToken: String!) {
+    //       authenticationVsCode(vsCodeToken: $_vsCodeToken) {
+    //         vsCodeToken
+    //         githubId
+    //         idToken
+    //         refreshToken
+    //       }
+    //     }`,
+    //   variables: { _vsCodeToken: token },
+    // };
+
+    const url = 'https://dxmhv6e367.execute-api.us-east-1.amazonaws.com/Stage';
+    request.get(
+      {
+        url: url + `/authenticationVSCode/vsCodeToken/${token}`,
+      },
+      async (error, response, body) => {
+        console.log('response', response);
+        console.log('body', body);
+        console.log('error', error);
+        body = JSON.parse(body);
+        console.log(
+          'authentication vscode',
+          body?.authenticationVSCode?.vsCodeToken
+        );
+        console.log(
+          'authentication vscode if',
+          body?.authenticationVSCode?.vsCodeToken === token
+        );
+        if (
+          body?.authenticationVSCode &&
+          body?.authenticationVSCode?.vsCodeToken === token &&
+          body?.statusCode === 200
+        ) {
+          console.log('Correct token');
+          i++;
+          response = body?.authenticationVSCode;
+        }
+      }
+    );
+    // response = await axios.get(`url/authenticationVSCode/vsCodeToken/${token}`);
+    // if (
+    //   response?.data?.authenticationVSCode &&
+    //   Object.keys(response?.data?.authenticationVSCode).length
+    // ) {
+    //   i++;
+    // }
+    await sleep(3000);
+  }
+  console.log('End', response);
+  return response;
+};
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
