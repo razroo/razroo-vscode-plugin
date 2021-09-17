30c888f74411318c87411b9268d6ee098347ceaa
Federico Ferreyra Lombardi <federicoferreyra2@gmail.com>
2021-09-17 16:22:16 -0300

    Revert "This commit obtains all folders of the workspace and hash information with JWT and send to frontend, read a new attribute projectFileStructure to search the folder selected in frontend with a new function, inserts the files if exist the folder selected by user else insert in root workspace"
    This reverts commit 23df20b8a5f1782dd93583556f8617a7776c23f6.

--- src/extension.ts
+++ src/extension.ts
@@ -7,11 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 import * as AdmZip from 'adm-zip';
 import * as request from 'request';
 import * as http from 'http2';
-import {
-  existVSCodeAuthenticate,
-  getAuth0Url,
-  getDirectoriesWithoutPrivatePath,
-} from './utils';
+import { existVSCodeAuthenticate, getAuth0Url } from './utils';
 import {
   AUTH0URL,
   COMMAND_AUTH0_AUTH,
@@ -63,21 +59,7 @@ export async function activate(context: vscode.ExtensionContext) {
       const token = uuidv4();
       context.workspaceState.update(MEMENTO_RAZROO_ID_VS_CODE_TOKEN, token);
       const host = SOCKET_HOST;
-      // obtain full path of the folders of the workspace
-      const workspaceFolders = vscode.workspace.workspaceFolders?.map(
-        (folder) => {
-          return { name: folder.name, path: folder?.uri?.path };
-        }
-      );
-      // remove full path and obtain the private path for each folder
-      let privateDirectories: Array<string> = [];
-      workspaceFolders?.map((folder) => {
-        privateDirectories.push(
-          getDirectoriesWithoutPrivatePath(folder.path, folder.name)
-        );
-      });
-      console.log('privateDirectories', privateDirectories);
-      const loginUrl = getAuth0Url(token, host, privateDirectories);
+      const loginUrl = getAuth0Url(token, host);
 
       const httpServer = createServer();
       const io = new Server(httpServer, {
