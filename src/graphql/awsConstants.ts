238606d87e69f7674804300094433b5fa3bf32a5
Charlie Greenman <charlie@razroo.com>
2021-10-19 03:43:27 +0300

    Update the auth url so it works as expected.

--- src/graphql/awsConstants.ts
+++ src/graphql/awsConstants.ts
@@ -2,4 +2,4 @@ export const AWS_ACCESS_KEY_ID = 'AKIARH4CZJQVHSVPLLNQ';
 export const AWS_SECRET_ACCESS_KEY = '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';
 export const REGION = 'us-east-1';
 export const URL_GRAPHQL = 'https://vuerbsj4cjffvfzx7cph4iy7se.appsync-api.us-east-1.amazonaws.com/graphql';
-export const URL_API_GATEWAY = 'https://hfivopxdd2.execute-api.us-east-1.amazonaws.com/Stage';
+export const URL_API_GATEWAY = 'https://3dx7pdrw13.execute-api.us-east-1.amazonaws.com/Stage';
