cd3df67fd6b7444b0f28ecec21f556ab52f230d2
Charlie Greenman <charlie@razroo.com>
2021-11-28 11:55:47 +0200

    ZETA-877: Benchmark progress towards fixing jest.

--- tests/graphql.test.ts
+++ tests/graphql.test.ts
@@ -1,31 +1,29 @@
-import { updatePrivateDirectoriesRequest } from "../../utils/graphql.utils";
+import { getFileS3 } from './../src/utils/request.utils';
+import { URL_GRAPHQL } from './../src/graphql/awsConstants';
+import { updatePrivateDirectoriesRequest } from './../src/utils/graphql.utils';
+import { mockAxios, getMockIdToken, getVsCodeAuthenticationMock } from './test-utils';
 import MockAdapter from 'axios-mock-adapter';
-import { URL_GRAPHQL } from '../../graphql/awsConstants';
-import { mockAxios, getMockIdToken, getVsCodeAuthenticationMock } from "./utils/utils";
-import { expect } from "chai";
-import { beforeEach } from "mocha";
-import { getFileS3 } from "../../utils/request.utils";
 
 const mock = new MockAdapter(mockAxios);
 const vsCodeAuthenticationObject = getVsCodeAuthenticationMock();
 const idToken = getMockIdToken();
 
-suite('Graphql tests', () => {
+describe('Graphql tests', () => {
 
 	beforeEach(() => {
         mock.reset();
 	});
 
-	test('Should update the privateDirectories attribute of the vscode authentication table with execute_test_privateDirectories', () => {
+	it('Should update the privateDirectories attribute of the vscode authentication table with execute_test_privateDirectories', async() => {
 		const privateDirectories = "execute_test_privateDirectories";
         mock.onPost(URL_GRAPHQL).reply(200, vsCodeAuthenticationObject);
-		updatePrivateDirectoriesRequest({
+		await updatePrivateDirectoriesRequest({
 			vsCodeToken: vsCodeAuthenticationObject.vsCodeInstanceId,
 			idToken, 
 			privateDirectories
 		}).then(response => {
-            expect(response).equal(vsCodeAuthenticationObject);
-			expect(mock.history.post.length).equal(1);
+            expect(response).toEqual(vsCodeAuthenticationObject);
+			expect(mock.history.post.length).toEqual(1);
         });
 	});
 	
@@ -35,8 +33,8 @@ suite('Graphql tests', () => {
         };
         mock.onPost(URL_GRAPHQL).reply(200, data);
         getFileS3({url: 'urlTest'}).then(response => {
-            expect(Object.keys(response)).to.have.lengthOf.greaterThan(0);
-            expect(mock.history.post.length).equal(1);
+            expect(Object.keys(response).length).toBeGreaterThan(0);
+            expect(mock.history.post.length).toEqual(1);
         });
 	});
 });
