1a82bf5a665fd1e2f307bc3d421cbefd35b92efe
Charlie Greenman <charlie@razroo.com>
2022-01-05 13:15:30 +0200

    ZETA-990: Complete logic for calling a command from within the vscode terminal.

--- src/utils/terminal.utils.ts
+++ src/utils/terminal.utils.ts
@@ -1,6 +1,6 @@
 import * as cp from "child_process";
 
-function execShell(cmd: string) {
+async function execShell(cmd: string) {
     return new Promise<string>((resolve, reject) => {
         cp.exec(cmd, (err, out) => {
             if (err) {
@@ -9,4 +9,10 @@ function execShell(cmd: string) {
             return resolve(out);
         });
     });
-} 
+}
+
+//example, will be removed once fully integrated
+const currentDir = execShell('pwd').then(data => {
+    console.log('data');
+    console.log(data);
+});
