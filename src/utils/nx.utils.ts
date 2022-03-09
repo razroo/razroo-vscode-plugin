4de851bca438f106885c3f63f6329788670d7121
Charlie Greenman <charlie@razroo.com>
2022-03-10 00:42:50 +0200

    ZETA-1425: Add nxJson util.

---
+++ src/utils/nx.utils.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { readPackageJson } from '@razroo/razroo-devkit';
+
+export async function readNxJson(workspacePath: string) {
+    const nxJsonFilePath = path.join(workspacePath, 'nx.json');
+    // official function is for packageJson, but will work for nxJson as well
+    // making as any for now to cancel out
+    const nxJson = await readPackageJson(nxJsonFilePath) as any;
+    // if nxJson is empty make sure returns undefined
+    if(!nxJson) {
+      return undefined;
+    }
+
+    const defaultProject = nxJson.defaultProject;
+    return {
+      defaultProject  
+    };
+}
