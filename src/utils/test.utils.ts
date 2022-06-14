c25c02c540b6e108295e5a4ddccaff48cff12eb0
Charlie Greenman <charlie@razroo.com>
2022-06-14 11:06:19 +0300

    ZETA-2368: Benchmark progress towards creating functionality for getting return result of test.

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -9,10 +9,31 @@ export async function unitTestGeneratedFiles (tempFiles: string[], folderName: s
         const unitTestFilePath = path.join(folderName, path.basename(file));
         const execution = new vscode.ShellExecution(`npm run test -- --test-file ${unitTestFilePath}`);
         const task = new vscode.Task({ type: "shell" }, vscode.TaskScope.Workspace, 'Razroo Terminal', 'Razroo', execution);
-        vscode.tasks.executeTask(task);
+        // These two functions in tandem allow us to figure out when task completed
+        const buildTasks = getBuildTasks();
+        await executeBuildTask(task, path.basename(file));
       }
     }));
-
-    showInformationMessage('Razroo unit tests have run successfully.');
 }
 
+async function executeBuildTask(task: vscode.Task, fileName) {
+    const execution = await vscode.tasks.executeTask(task);
+
+    return new Promise<void>(resolve => {
+        let disposable = vscode.tasks.onDidEndTask(e => {
+            if (e.execution === execution) {
+                showInformationMessage(`${fileName} has run successfully.`);
+                // disposable.dispose();
+                resolve();
+            }
+        });
+    });
+}
+
+async function getBuildTasks() {
+    return new Promise<vscode.Task[]>(resolve => {
+        vscode.tasks.fetchTasks().then((tasks) => {
+            resolve(tasks.filter((task) => task.group === vscode.TaskGroup.Build));
+        });
+    });
+}
