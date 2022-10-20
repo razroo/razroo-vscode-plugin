017de9b59b732c5cb41046ed08ff1f89d7383807
Charlie Greenman <charlie@razroo.com>
2022-10-20 18:48:05 +0300

    ZETA-3673: Add authentication functionality for vscode.

---
+++ src/utils/authentication/authentication.ts
@@ -0,0 +1,6 @@
+import { RAZROO_URL, RAZROO_DEV_URL } from './../../constants';
+
+export const getAuth0Url = (isProduction: boolean) => {
+  const host = isProduction === true ? RAZROO_URL : RAZROO_DEV_URL;
+  return `${host}/vscode-auth`;
+};
