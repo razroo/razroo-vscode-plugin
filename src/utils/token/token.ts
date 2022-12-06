8ee9b6aa0116e7bf4561220afb2d051536db3c37
Charlie Greenman <charlie@razroo.com>
2022-12-06 17:59:49 +0200

    ZETA-4147: Add extract project name from codemod into vscodeInstanceId logic.

--- src/utils/token/token.ts
+++ src/utils/token/token.ts
@@ -2,15 +2,16 @@ import parseGitConfig from 'parse-git-config';
 import { EMPTY, MEMENTO_RAZROO_ID_VS_CODE_TOKEN } from './../../constants';
 import * as vscode from 'vscode';
 import { v4 as uuidv4 } from 'uuid';
+import { extractProjectName } from '@razroo/razroo-codemod';
 
 export async function getOrCreateAndUpdateIdToken(context: vscode.ExtensionContext): Promise<string> {
     let token: string | undefined = context.workspaceState.get(MEMENTO_RAZROO_ID_VS_CODE_TOKEN);
     if (!token) {
       const workspacePath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
       const gitOrigin = await parseGitConfig({ cwd: workspacePath, path: '.git/config' }).then(gitConfig => {
-        return gitConfig?.['remote "origin"']?.url
+        return gitConfig?.['remote "origin"']?.url;
       });
-      token = gitOrigin ? gitOrigin : EMPTY;
+      token = gitOrigin ? extractProjectName(gitOrigin) : EMPTY;
       context.workspaceState.update(MEMENTO_RAZROO_ID_VS_CODE_TOKEN, token);
       return token as string;
     }
