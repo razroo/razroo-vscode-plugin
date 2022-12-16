67e9882ca45afb7e5d60e262992368118ea33877
Charlie Greenman <charlie@razroo.com>
2022-12-16 16:21:24 +0200

    ZETA-4248: Benchmarkk progress towards completing infeatsrurcte command path.

--- src/utils/command/command.ts
+++ src/utils/command/command.ts
@@ -1,3 +1,12 @@
+import * as vscode from 'vscode';
+
 export function containsInfrastructureCommandPath(text: string) : boolean {
   return text.includes('<%= infrastructureCommandPath %>');
+}
+
+export function openWorkspaceInNewCodeEditor(infastructureCommandPath: string, fileName: string) {
+  const commandToExecute = infastructureCommandPath ? `open -a "Visual Studio Code" ${infastructureCommandPath}/${fileName}` : `open -a "Visual Studio Code" ${fileName}`;
+  const execution = new vscode.ShellExecution(commandToExecute);
+  const task = new vscode.Task({ type: "shell" }, vscode.TaskScope.Workspace, 'Razroo Terminal', 'Razroo', execution);
+  vscode.tasks.executeTask(task);  
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -24,6 +24,7 @@ import { isTokenExpired } from './date/date.utils';
 import { integrationTestGeneratedFiles, unitTestGeneratedFiles } from './test.utils';
 import { join, extname} from 'path';
 import { determineFilePathParameter, determineType, effects, getAllDirectoriesFromVsCodeFolder, getVersionAndNameString, replaceCurlyBrace } from '@razroo/razroo-codemod';
+import { containsInfrastructureCommandPath, openWorkspaceInNewCodeEditor } from './command/command';
 
 const showInformationMessage = vscode.window.showInformationMessage;
 
@@ -71,6 +72,9 @@ export const saveFiles = async (
       const execution = new vscode.ShellExecution(commandToExecute);
       const task = new vscode.Task({ type: "shell" }, vscode.TaskScope.Workspace, 'Razroo Terminal', 'Razroo', execution);
       await vscode.tasks.executeTask(task);
+      if(containsInfrastructureCommandPath(commandToExecute)) {
+        openWorkspaceInNewCodeEditor(parametersParsed.infastructureCommandPath, parametersParsed.fileName);
+      }
       showInformationMessage('Command run in terminal');
     }
 
