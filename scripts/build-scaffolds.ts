0bbf75a7c1bed0d89ed34bfa729b7ba4339968f6
Charlie Greenman <charlie@razroo.com>
2023-01-15 15:33:09 +0200

    ZETA-4420: Add build to scaffold sub menu for the razroo vscode plugin.

--- scripts/build-scaffolds.ts
+++ scripts/build-scaffolds.ts
@@ -3,6 +3,7 @@ import { COMMUNITY } from '../src/constants';
 import { getPaths } from '../src/graphql/get-paths/paths.service';
 import path from "path";
 import dotenv from "dotenv";
+import { createScaffoldSubmenu } from '../src/utils/scaffold/scaffold';
 // Parsing the env file.
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -16,11 +17,20 @@ const accessToken = process.env.accessToken as string;
 const production = true;
 
 getPaths(COMMUNITY, accessToken, production).then(paths => {
+  const scaffoldSubmenu = [] as any;
   const angularPath = paths[0];
   console.log('angularPath');
   console.log(angularPath);
   getPathScaffolds(angularPath.orgId, angularPath.id, accessToken, production).then(scaffolds => {
+    scaffolds.forEach(scaffold => {
+      const createScaffoldSubmenuItem = createScaffoldSubmenu(scaffold.pathId, scaffold.id);
+      scaffoldSubmenu.push(createScaffoldSubmenuItem);
+    });
     console.log('scaffolds');
     console.log(scaffolds);
+    console.log('scaffoldSubmenu');
+    console.log(scaffoldSubmenu);
   });
+
+  
 });
