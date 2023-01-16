0a3d9b3be660f01a9d04aa7afa4798f5838392e9
Charlie Greenman <charlie@razroo.com>
2023-01-16 16:55:09 +0200

    ZETA-4442: Benchmark progress towards completing pathId logic for commands so more genertic.

--- scripts/build-scaffolds.ts
+++ scripts/build-scaffolds.ts
@@ -32,8 +32,8 @@ getPaths(COMMUNITY, accessToken, production).then(paths => {
   getPathScaffolds(angularPath.orgId, angularPath.id, accessToken, production).then(scaffolds => {
     const pathId = getVersionAndNameString(angularPath.id);
     scaffolds.forEach(scaffold => {
-      const createScaffoldSubmenuItem = createScaffoldSubmenu(scaffold.pathId, scaffold.id);
-      const createScaffoldCommandItem = createScaffoldCommand(scaffold.pathId, scaffold.id);
+      const createScaffoldSubmenuItem = createScaffoldSubmenu(pathId.name, scaffold.id);
+      const createScaffoldCommandItem = createScaffoldCommand(pathId.name, scaffold.id);
       scaffoldSubmenu.push(createScaffoldSubmenuItem);
       scaffoldCommands.push(createScaffoldCommandItem);
       const pushScaffoldFunctionStatement = buildScaffoldFunctionStatement(pathId.name, scaffold.id, scaffold.recipeId);
