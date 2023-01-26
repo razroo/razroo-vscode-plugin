a62200be643f8d5a080eefb15aa46a204e154b3e
Charlie Greenman <charlie@razroo.com>
2023-01-26 03:49:37 +0200

    Add react and vue scaffolds.

--- src/utils/scaffold/push-scaffold-commands.ts
+++ src/utils/scaffold/push-scaffold-commands.ts
@@ -49,6 +49,41 @@ function generateNgrxNgrxFeature(vscode, context, isProduction, packageJsonParam
   );
 }
 
+function generateReactReactComponent(vscode, context, isProduction, packageJsonParams) {
+  return vscode.commands.registerCommand(
+    'generate.react.reactComponent',
+    async ({ path }) => createScaffold(vscode, 'react-18.2.0', 'react-scaffolds', path, context, isProduction, 'react-component', packageJsonParams)
+  );
+}
+
+function generateReactReactReduxSlice(vscode, context, isProduction, packageJsonParams) {
+  return vscode.commands.registerCommand(
+    'generate.react.reactReduxSlice',
+    async ({ path }) => createScaffold(vscode, 'react-18.2.0', 'react-scaffolds', path, context, isProduction, 'react-redux-slice', packageJsonParams)
+  );
+}
+
+function generateVueComponent(vscode, context, isProduction, packageJsonParams) {
+  return vscode.commands.registerCommand(
+    'generate.vue.component',
+    async ({ path }) => createScaffold(vscode, 'vue-3.2.0', 'vue-scaffolds', path, context, isProduction, 'component', packageJsonParams)
+  );
+}
+
+function generateVueView(vscode, context, isProduction, packageJsonParams) {
+  return vscode.commands.registerCommand(
+    'generate.vue.view',
+    async ({ path }) => createScaffold(vscode, 'vue-3.2.0', 'vue-scaffolds', path, context, isProduction, 'view', packageJsonParams)
+  );
+}
+
+function generateVuePiniaStore(vscode, context, isProduction, packageJsonParams) {
+  return vscode.commands.registerCommand(
+    'generate.vue.piniaStore',
+    async ({ path }) => createScaffold(vscode, 'vue-3.2.0', 'vue-scaffolds', path, context, isProduction, 'pinia-store', packageJsonParams)
+  );
+}
+
 export function pushScaffoldCommands(context, vscode, isProduction: boolean, packageJsonParams) {
-  context.subscriptions.push(generateAngularComponent(vscode, context, isProduction, packageJsonParams), generateAngularAngularService(vscode, context, isProduction, packageJsonParams), generateAngularAngularGuard(vscode, context, isProduction, packageJsonParams), generateAngularTypescriptInterface(vscode, context, isProduction, packageJsonParams), generateAngularAngularPipe(vscode, context, isProduction, packageJsonParams), generateAngularAngularDirective(vscode, context, isProduction, packageJsonParams), generateNgrxNgrxFeature(vscode, context, isProduction, packageJsonParams))
+  context.subscriptions.push(generateAngularComponent(vscode, context, isProduction, packageJsonParams), generateAngularAngularService(vscode, context, isProduction, packageJsonParams), generateAngularAngularGuard(vscode, context, isProduction, packageJsonParams), generateAngularTypescriptInterface(vscode, context, isProduction, packageJsonParams), generateAngularAngularPipe(vscode, context, isProduction, packageJsonParams), generateAngularAngularDirective(vscode, context, isProduction, packageJsonParams), generateNgrxNgrxFeature(vscode, context, isProduction, packageJsonParams), generateReactReactComponent(vscode, context, isProduction, packageJsonParams), generateReactReactReduxSlice(vscode, context, isProduction, packageJsonParams), generateVueComponent(vscode, context, isProduction, packageJsonParams), generateVueView(vscode, context, isProduction, packageJsonParams), generateVuePiniaStore(vscode, context, isProduction, packageJsonParams))
 }
