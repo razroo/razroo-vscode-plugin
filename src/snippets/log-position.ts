610c565e12ef816cda29094acfd2a0a606c833d7
Charlie Greenman <charlie@razroo.com>
2023-03-05 16:14:55 +0200

    ZETA-4831: Complete code snippet to determine if a comment has been written.

--- src/snippets/log-position.ts
+++ src/snippets/log-position.ts
@@ -5,6 +5,15 @@ export function logCursorPosition(selection: vscode.Selection) {
   const lineNumber = selection.active.line + 1;
   const columnNumber = selection.active.character + 1;
   const lineText = editor.document.lineAt(lineNumber - 1).text.trim();
-  console.log(`Cursor position: line ${lineNumber}, column ${columnNumber}`);
-  console.log(`lineText: ${lineText}`);
+  if (isComment(lineText)) {
+    console.log(`Cursor position: line ${lineNumber}, column ${columnNumber}`);
+    console.log(`lineText: ${lineText}`);
+  }
+  
+}
+
+function isComment(lineText: string): boolean {
+  // Regex pattern to match comment styles across different programming languages
+  const commentRegex = /^(\s*\/\/)|(^#)|(^\/\*)|(^<!--)/;
+  return commentRegex.test(lineText);
 }
