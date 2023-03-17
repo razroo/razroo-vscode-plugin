51f636773788aa37a580d88d4f14b86dd0203641
Charlie Greenman <charlie@razroo.com>
2023-03-17 14:46:02 +0200

    ZETA-4945: Add styling for vscode to make it look good.

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -31,6 +31,11 @@ export class ProjectsWebview implements vscode.WebviewViewProvider {
 
     private _getHtmlForWebview(webview: vscode.Webview): string {
         const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+        // Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
         // Use a nonce to only allow a specific script to be run.
 		const nonce = getNonce();
         return `
@@ -38,18 +43,27 @@ export class ProjectsWebview implements vscode.WebviewViewProvider {
             <html>
             <head>
                 <meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading styles from our extension directory,
+					and only allow scripts that have a specific nonce.
+					(See the 'webview-sample' extension sample for img-src content security policy examples)
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Razroo Projects</title>
-                // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
 
+                <title>Razroo Projects</title>
             </head>
             <body>
+                <h3>Click on the GO! button, to connect your VSCode Editor to Razroo</h3>
                 <select>
                     <option value="option1">Option 1</option>
                     <option value="option2">Option 2</option>
                     <option value="option3">Option 3</option>
                 </select>
-                <button>Submit</button>
+                <button>GO!</button>
                 <script nonce="${nonce}" src="${scriptUri}"></script>
             </body>
             </html>
