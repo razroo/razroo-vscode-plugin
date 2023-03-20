1af0c50b7d623ff1f01c9422c35fe769149cb61d
Charlie Greenman <charlie@razroo.com>
2023-03-20 17:38:28 +0200

    ZETA-4968: Add git config logic.

--- src/projects/git-configs.ts
+++ src/projects/git-configs.ts
@@ -1,13 +1,26 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import ignore from 'ignore';
 
 export async function getAllGitConfigs(dir: string): Promise<Record<string, string>[]> {
   const configs: Record<string, string>[] = [];
+  // Load the root .gitignore file, if it exists
+  let ignorePatterns: string[] = [];
+  const gitignorePath = path.join(dir, '.gitignore');
+  if (await fileExists(gitignorePath)) {
+    ignorePatterns = await getGitignorePatterns(gitignorePath);
+  }
 
   const subdirs = fs.readdirSync(dir, { withFileTypes: true });
   for (const subdir of subdirs) {
+    const filePath = path.join(dir, subdir.name);
+    if (ignorePatterns.some((pattern) => {
+      return filePath.startsWith(filePath) && subdir.name !== '.git';
+    })) {
+      continue;
+    }
     if (subdir.isDirectory()) {
-      const subdirConfigs = await getAllGitConfigs(path.join(dir, subdir.name));
+      const subdirConfigs = await getAllGitConfigs(filePath);
       configs.push(...subdirConfigs);
     } else if (subdir.isFile() && subdir.name === 'config') {
       const configPath = path.join(dir, subdir.name);
@@ -32,4 +45,49 @@ export async function getAllGitConfigs(dir: string): Promise<Record<string, stri
   }
 
   return configs;
+}
+
+async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    await fs.promises.access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function getGitignorePatterns(gitignorePath: string): Promise<string[]> {
+  const gitignoreContent = await fs.promises.readFile(gitignorePath, 'utf8');
+  return gitignoreContent
+    .split(/\r?\n/)
+    .filter((line) => line.trim() !== '' && !line.trim().startsWith('#'))
+    .map((line: any) => path.join(path.dirname(gitignorePath), line));
+}
+
+export async function findGitFolders(parentFolder: string, gitFolders: any[] = []): Promise<string[]> {
+  // Create an ignore object based on the parentFolder's .gitignore file
+  const ignoreRules = fs.readFileSync(path.join(parentFolder, '.gitignore'), 'utf8');
+  const ig = ignore().add(ignoreRules);
+
+  async function search(folder: string) {
+    const files = fs.readdirSync(folder);
+
+    for (const file of files) {
+      const filePath = path.join(folder, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        // Check if the folder is gitignored
+        if (!ig.filter([filePath]).length) {
+          if (file === '.git') {
+            gitFolders.push(filePath);
+          } else {
+            return await search(filePath);
+          }
+        }
+      }
+    }
+  };
+
+  return await search(parentFolder);
 }
