304b96084edace80f2687c214f83b563ddc8a9af
Charlie Greenman <charlie@razroo.com>
2023-03-25 20:56:16 +0300

    ZETA-5000: Add a v1 for projects scaffolds to use non map flavored package json.

--- src/scaffolds/determine-languages-used.ts
+++ src/scaffolds/determine-languages-used.ts
@@ -4,10 +4,14 @@ import { coreProgrammingLanguagesMap } from "package-json-manager";
 export async function determineLanguagesUsed(packageJson: PackageJson): Promise<string[]> {
     const languagesUsedArr = [] as any;
     const coreProgrammingLanguages = coreProgrammingLanguagesMap();
-    // TODO
     for(const dependency in packageJson.dependencies) {
       if(coreProgrammingLanguages[dependency]) {
         languagesUsedArr.push(coreProgrammingLanguages[dependency]);
+      }  
+    }
+    for(const devDependency in packageJson.devDependencies) {
+      if(coreProgrammingLanguages[devDependency]) {
+        languagesUsedArr.push(coreProgrammingLanguages[devDependency]);
       }  
     }
   
