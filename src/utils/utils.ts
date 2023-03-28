20fa5452e80d08dcd9d9a156a4c507f3767fd962
Charlie Greenman <charlie@razroo.com>
2023-03-28 22:44:16 +0300

    Zeta 5038 kebab case file names (#205)
    * ZETA-5038: Add kebab case logic for files generated.
    
    * ZETA-5038: Remove curly brace true for filenName as no need and might add extra compute.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -68,7 +68,7 @@ export const saveFiles = async (
   }
   for await (const zipEntry of zipEntries) {
     const parametersParsed = JSON.parse(parameters);
-    const fileNameandPath = normalize(replaceCurlyBrace(parametersParsed, zipEntry.entryName));
+    const fileNameandPath = normalize(replaceCurlyBrace(parametersParsed, zipEntry.entryName, true));
     const fileName = replaceCurlyBrace(parametersParsed, zipEntry.name);
 
     if (extname(fileName) === ".sh") {
