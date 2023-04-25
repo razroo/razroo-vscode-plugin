74d3cbf85d0437abce7df37d07c3b2940b50df22
Charlie Greenman <charlie@razroo.com>
2023-04-25 12:36:49 +0300

    Push through name js fix.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports =  function withDefaults(extConfig) {
         entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
         output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
             path: path.resolve(__dirname, 'dist'),
-            filename: 'extension.js',
+            filename: `[name].js`,
             libraryTarget: "commonjs2",
             devtoolModuleFilenameTemplate: '../[resource-path]'
         },
