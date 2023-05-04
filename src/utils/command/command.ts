ccc5050dd1e128fbd79bb6affe503cf94b9485d8
Charlie Greenman <charlie@razroo.com>
2023-05-04 13:15:52 +0300

    Zeta 5343 - We did it. upload terminal output txt file (#239)
    * ZETA-5343: Benchmark progress towarsds completing the terminal u preview logic.
    
    * ZETA-5343: Benchmark progress towards completing the command output logic.
    
    * ZETA-5343: Benchmark progress towards having the terminal file being outputted.
    
    * ZETA-5343: Complete preview logic for uploading commands for razroo.
    
    * ZETA-5343: Complete terminal command output txt preview.

--- src/utils/command/command.ts
+++ src/utils/command/command.ts
@@ -1,16 +1,22 @@
 import { setCommandStatus } from '../../utils/graphql.utils';
-import { DEV_APP_URL, PROD_APP_URL } from '../../constants';
+import { DEV_APP_URL, MEMENTO_RAZROO_ACCESS_TOKEN, PROD_APP_URL } from '../../constants';
 import * as vscode from 'vscode';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { uploadPreviewFile } from '../../preview/preview.mutations';
+import { MEMENTO_RAZROO_ORG_ID } from '../../constants';
 
-export async function runRazrooCommand(commandToExecute: string, parametersParsed: any,isProduction: any, template: any, runPreviewGeneration: boolean) {  
+export async function runRazrooCommand(commandToExecute: string, parametersParsed: any,isProduction: any, template: any, runPreviewGeneration: boolean, folderRoot: string, context: any) {  
   let execution;
+  const userOrgId = context.globalState.get(MEMENTO_RAZROO_ORG_ID);
+  const accessToken = context.globalState.get(MEMENTO_RAZROO_ACCESS_TOKEN);
   if(runPreviewGeneration) {
     execution = new vscode.ShellExecution(commandToExecute + ' >> terminal-output.txt');
   } else {
     execution = new vscode.ShellExecution(commandToExecute);
   }
   const task = new vscode.Task({ type: "shell" }, vscode.TaskScope.Workspace, 'Razroo Terminal', 'Razroo', execution);
-  await executeCommandTask(task, parametersParsed, isProduction, template);
+  await executeCommandTask(task, parametersParsed, isProduction, template, runPreviewGeneration, folderRoot, userOrgId, accessToken);
 }
 
 export function containsInfrastructureCommandPath(parametersParsed: any): boolean {
@@ -24,7 +30,7 @@ export async function openWorkspaceInNewCodeEditor(fileName: string, infrastruct
   await vscode.tasks.executeTask(task);  
 }
 
-async function executeCommandTask(task: vscode.Task, parametersParsed: any,isProduction: any, template: any) {
+async function executeCommandTask(task: vscode.Task, parametersParsed: any,isProduction: any, template: any, runPreviewGeneration: boolean, folderRoot: string, userOrgId: string, accessToken: string) {
     const execution = await vscode.tasks.executeTask(task);
 
     const {orgId, pathId, recipeId, id } = template;
@@ -47,6 +53,14 @@ async function executeCommandTask(task: vscode.Task, parametersParsed: any,isPro
                 if(containsInfrastructureCommandPath(parametersParsed)) {
                   await openWorkspaceInNewCodeEditor(parametersParsed.fileName, parametersParsed.infrastructureCommandPath);
                   resolve();
+                } else if(runPreviewGeneration) {
+                  const commandOutputFile = join(folderRoot, 'terminal-output.txt');
+                  let commandOutputFileText = readFileSync(commandOutputFile, 'utf-8');
+                  await uploadPreviewFile(userOrgId, template.orgId, commandOutputFileText, 
+                    'terminal-output.txt', '.txt', template.pathId, 
+                    template.recipeId, template.id, 
+                    isProduction, accessToken);
+                  vscode.window.showInformationMessage('Preview for command has been uploaded');
                 }
                 else {
                   resolve();
