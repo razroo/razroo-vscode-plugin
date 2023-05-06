1e3b07276584c7a0c50b6df6092bc2d2bef93ca2
Charlie Greenman <charlie@razroo.com>
2023-05-07 02:33:10 +0300

    ZETA-5369: Update expired logic.

--- src/graphql/expired.ts
+++ src/graphql/expired.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { COMMAND_AUTH0_AUTH, MEMENTO_RAZROO_ACCESS_TOKEN, MEMENTO_RAZROO_REFRESH_TOKEN } from "../constants";
 import jwt_decode from "jwt-decode";
-import { auth0Client } from '../utils/graphql.utils';
+import { refreshAuth0Token } from '../utils/graphql.utils';
 
 /** 
  * isTokenExpired - Determines if accessToken is expired
@@ -18,24 +18,28 @@ export function isTokenExpired(accessToken: string): boolean {
   }
 }
 
-async function refreshAccessToken(context, isProduction: boolean) {
-    const refreshToken = await context.globalState.get(MEMENTO_RAZROO_REFRESH_TOKEN);
-    return auth0Client(isProduction).refreshToken({ refresh_token: refreshToken }, async function (err, userData) {
-      if (err) {
-        vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
-      }
-  
-      await context.globalState.update(MEMENTO_RAZROO_ACCESS_TOKEN, userData.access_token);
-      await context.globalState.update(MEMENTO_RAZROO_REFRESH_TOKEN, userData.refresh_token);
-      await vscode.window.showInformationMessage('User successfully authenticated with Razroo.');
-      return userData.access_token;
-    });
-  };
+async function refreshAccessToken(context: vscode.ExtensionContext, isProduction: boolean): Promise<any> {
+  try {
+    const refreshToken = await context.globalState.get(MEMENTO_RAZROO_REFRESH_TOKEN) as string;
+    const userData = await refreshAuth0Token(refreshToken, isProduction);
+
+    await context.globalState.update(MEMENTO_RAZROO_ACCESS_TOKEN, userData.access_token);
+    await context.globalState.update(MEMENTO_RAZROO_REFRESH_TOKEN, userData.refresh_token);
+    await vscode.window.showInformationMessage('User successfully authenticated with Razroo.');
+    return userData.access_token;
+  } catch (err) {
+    console.log('refresh token error');
+    console.log(err);
+    vscode.commands.executeCommand(COMMAND_AUTH0_AUTH);
+    return undefined;
+  }
+};
+
 
 export async function getAccessToken(context: vscode.ExtensionContext, isProduction: boolean) {
   const accessToken = context.globalState.get(MEMENTO_RAZROO_ACCESS_TOKEN) as string;
   if (isTokenExpired(accessToken)) {
-    const newToken = await refreshAccessToken(accessToken, isProduction);
+    const newToken = await refreshAccessToken(context, isProduction);
     return newToken;
   }
   return accessToken;
