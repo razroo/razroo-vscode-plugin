6c3ecf789932f90b5e563952e82b732080515686
Charlie Greenman <charlie@razroo.com>
2023-05-21 00:34:06 +0300

    ZETA-5533: VSCode now pass uuid to auth login for the sake of subscribe mutation. (#257)

--- src/update-vscode/update-vscode.ts
+++ src/update-vscode/update-vscode.ts
@@ -7,7 +7,7 @@ import { getAuth0Url } from '../utils/authentication/authentication';
 import { createVSCodeIdToken } from '../utils/token/token';
 import { ProjectConfig } from '../projects/interfaces/project-config.interfaces';
 import { getUserOrganizations } from '../projects/organizations/organizations.service';
-
+import { v4 as uuidv4 } from 'uuid';
 
 export async function updateVsCode(context: vscode.ExtensionContext, isProduction: boolean, selectedProjects: ProjectConfig[], projectsProvider: any) {
     const loginUrl = getAuth0Url(isProduction);
@@ -15,14 +15,15 @@ export async function updateVsCode(context: vscode.ExtensionContext, isProductio
     let isInProgress = true;
     let disposeServer;
     try {
-        isInProgress && await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(loginUrl));
+        const uuid = uuidv4();
+        const urlWithUuid = `${loginUrl}/${uuid}`;
+        isInProgress && await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(urlWithUuid));
         const { createServerPromise, dispose } = createDisposableAuthServer();
         disposeServer = dispose;
         const { accessToken = '', refreshToken = '', userId = '', orgId = '' } = isInProgress ? await createServerPromise : {};
         setWorkspaceState(context, accessToken, refreshToken, userId, orgId, isInProgress);
         if(isInProgress) {
           if(selectedProjects) {
-            console.log('selected projects called');
             for(let selectedProject of selectedProjects) {
               const vsCodeInstanceId = createVSCodeIdToken(userId, selectedProject.versionControlParams);
               await updatePrivateDirectoriesInVSCodeAuthentication(accessToken, isProduction, userId, orgId, selectedProjects);
