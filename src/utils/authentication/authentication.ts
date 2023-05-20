4d13d99574f1b1b348a63ef6d656007bccb30373
Charlie Greenman <charlie@razroo.com>
2023-05-21 00:32:42 +0300

    ZETA-5533: VSCode now pass uuid to auth login for the sake of subscribe mutation.

--- src/utils/authentication/authentication.ts
+++ src/utils/authentication/authentication.ts
@@ -2,7 +2,7 @@ import { RAZROO_URL, RAZROO_DEV_URL } from './../../constants';
 
 export const getAuth0Url = (isProduction: boolean) => {
   const host = isProduction === true ? RAZROO_URL : RAZROO_DEV_URL;
-  return `${host}/api/auth/login`;
+  return `${host}/api/auth/login?returnTo=/success`;
 };
 
 export const getAuth0LogoutUrl = (isProduction: boolean) => {
