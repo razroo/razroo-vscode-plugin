41b50dd0630d6b8a43d95f328c77cdd021711d06
Charlie Greenman <charlie@razroo.com>
2023-05-21 16:16:38 +0300

    ZETA-5535: Subscription auth data.

--- src/graphql/subscription.ts
+++ src/graphql/subscription.ts
@@ -9,10 +9,11 @@ import { URL_GRAPHQL, REGION, URL_PROD_GRAPHQL } from './awsConstants';
 // Require AppSync module
 import { AWSAppSyncClient, AUTH_TYPE } from 'aws-appsync';
 
-const type = AUTH_TYPE.OPENID_CONNECT;
+
 
 // Set up Apollo client
 function client(accessToken: string, isProduction: boolean) {
+  const type = AUTH_TYPE.OPENID_CONNECT;
   return new AWSAppSyncClient({
     url: isProduction === true ? URL_PROD_GRAPHQL : URL_GRAPHQL,
     region: REGION,
@@ -25,3 +26,17 @@ function client(accessToken: string, isProduction: boolean) {
 }
 
 export default client;
+
+// Set up Apollo client
+export function authDataClient(isProduction: boolean) {
+  const type = AUTH_TYPE.API_KEY;
+  return new AWSAppSyncClient({
+    url: isProduction === true ? URL_PROD_GRAPHQL : URL_GRAPHQL,
+    region: REGION,
+    auth: {
+      type,
+      apiKey: 'da2-pineeux4cnaobnb5udeum2o5de'
+    },
+    disableOffline: true
+  });
+}
