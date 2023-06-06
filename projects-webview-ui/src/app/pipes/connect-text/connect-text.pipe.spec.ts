6fd43390a6d6b78ed47553eda0077537578d3ecc
Charlie Greenman <charlie@razroo.com>
2023-06-06 15:44:55 +0300

    ZETA-5695: Update the connect text pipe spec.

--- projects-webview-ui/src/app/pipes/connect-text/connect-text.pipe.spec.ts
+++ projects-webview-ui/src/app/pipes/connect-text/connect-text.pipe.spec.ts
@@ -5,4 +5,20 @@ describe('ConnectTextPipe', () => {
     const pipe = new ConnectTextPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should display connect if selecte projects not within original projects', () => {
+    const selectedProjects = [
+      {
+        id: '123'
+      }
+    ] as any;
+    const originalProjects = [
+      {
+        id: 'test123'
+      }
+    ] as any;
+    const pipe = new ConnectTextPipe();
+    const result = pipe.transform(selectedProjects, originalProjects);
+    expect(result).toEqual('Connect');
+  });
 });
