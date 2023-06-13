2a080179c545e3e90c37ece7b9b031e3b2e7a304
Charlie Greenman <charlie@razroo.com>
2023-06-13 17:58:18 +0300

    ZETA-5776: Add more unit test use case for personal workspace.

--- src/utils/token/token.spec.ts
+++ src/utils/token/token.spec.ts
@@ -1,20 +1,26 @@
 import { createVSCodeIdToken } from "./token";
 
 describe('createVSCodeIdToken',() => {
+  const versionControlParams = {
+    gitOrigin: 'git@github.com:razroo/razroo-vscode-plugin.git',
+    gitBranch: 'test-branch',
+    path: 'user/chill'
+  };
+  const packageJsonParams = {
+    name: 'razroo-angular-starter'
+  };
+  const userId = 'chill-mcchill-123';
+  const orgId = 'chill-mcchill-123';
   it('should extract id token as expected given git origin', () => {
-    const versionControlParams = {
-      gitOrigin: 'git@github.com:razroo/razroo-vscode-plugin.git',
-      gitBranch: 'test-branch',
-      path: 'user/chill'
-    };
-    const packageJsonParams = {
-      name: 'razroo-angular-starter'
-    };
-    const userId = 'chill-mcchill-123';
-    const orgId = 'chill-mcchill-123';
-
     const result = createVSCodeIdToken(userId, orgId, versionControlParams, packageJsonParams);
     const expected = 'chill-mcchill-123_razroo_razroo-vscode-plugin';
+    expect(result).toEqual(expected);
+  });
+
+  it('should return personal workspace token if personal workspace but no git origin', () => {
+    const versionControlParamsAlt = {} as any;
+    const result = createVSCodeIdToken(userId, orgId, versionControlParamsAlt, packageJsonParams);
+    const expected = 'chill-mcchill-123_razroo-angular-starter';
     expect(result).toEqual(expected);
   });
 });
