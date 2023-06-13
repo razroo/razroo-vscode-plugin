67e4a3b9ec746bbcee7bc85b4c9859aae556be0c
Charlie Greenman <charlie@razroo.com>
2023-06-13 17:55:55 +0300

    ZETA-5776: Benchmark progress towards completing the vscode token logic for personal workspace.

--- src/utils/token/token.spec.ts
+++ src/utils/token/token.spec.ts
@@ -1,5 +1,20 @@
+import { createVSCodeIdToken } from "./token";
+
 describe('createVSCodeIdToken',() => {
-  it('should create a vscode id token', () => {
-    expect(1).toEqual(1);
+  it('should extract id token as expected given git origin', () => {
+    const versionControlParams = {
+      gitOrigin: 'git@github.com:razroo/razroo-vscode-plugin.git',
+      gitBranch: 'test-branch',
+      path: 'user/chill'
+    };
+    const packageJsonParams = {
+      name: 'razroo-angular-starter'
+    };
+    const userId = 'chill-mcchill-123';
+    const orgId = 'chill-mcchill-123';
+
+    const result = createVSCodeIdToken(userId, orgId, versionControlParams, packageJsonParams);
+    const expected = 'chill-mcchill-123_razroo_razroo-vscode-plugin';
+    expect(result).toEqual(expected);
   });
 });
