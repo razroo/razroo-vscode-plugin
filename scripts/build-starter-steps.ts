6dd1c60965bfcf0e40d26ad7d91f0995cbaabb84
Charlie Greenman <charlie@razroo.com>
2023-06-29 18:11:00 +0300

    ZETA-5949: Update the build starter steps ts file.

--- scripts/build-starter-steps.ts
+++ scripts/build-starter-steps.ts
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'fs';
 import { getStarterSteps } from '../src/starters/get-starter-steps.service';
 
 const isProduction = true;
@@ -5,4 +6,5 @@ const isProduction = true;
 getStarterSteps(isProduction).then(starterSteps => {
   console.log('starterSteps');
   console.log(starterSteps);
+  writeFileSync('projects-webview-ui/src/app/data/starter-steps.ts', '{}');
 });
