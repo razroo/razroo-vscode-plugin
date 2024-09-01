1c62a684d678846a70767979dfc7062a0b0ccb97
Charlie Greenman <charlie@razroo.com>
2024-09-01 05:49:38 -0400

    add latest

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports =  function withDefaults(extConfig) {
                           }
                         }
                     }]
-                },
+                }
           ]
         },
         performance: {
